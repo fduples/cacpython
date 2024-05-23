e6d3b984cc077c388f53b0cdea2c5943eca2c93b
Fade-tor <72475945+Fade-tor@users.noreply.github.com>
2024-05-23 11:53:55 -0300

    inicio python

---
+++ python/inicio.py
@@ -0,0 +1,28 @@
+print("hola mundo")
+
+#Comentario una linea
+
+"""
+Comentario
+de
+varias
+lineas
+"""
+
+cadena = "cadena de caracteres"
+print(type(cadena))
+entero = 46
+print(type(entero))
+flotante = 4.5
+print(type(flotante))
+booleano = True
+print(type(booleano))
+
+#para ingresar valores por teclado
+
+pelicula = input("Ingrese el nombre de la pelica de la")
+print(pelicula)
+
+precio = 6000
+cantidad_entradas = int(input("Cuentas entradas va a comprar: "))
+monto_total = print(precio * cantidad_entradas)
